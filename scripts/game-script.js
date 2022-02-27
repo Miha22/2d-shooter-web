664b8b20e771df025d0c1f91ad417c7a5c8e0f8c
Deniel Alekseev <daniel.neo.eu@icloud.com>
2022-02-27 04:29:52 +0100

    Added Player and Projection classes, draw methods to depic player and trajectory using arc function of canvas. Added event listener on click that draws trajectory towards mouse click. Added trajection calculation using atan2 method that returns angle between click and player location in radians then by using cos and sin functions I can obtain values for velocity (x and y) that will be continuosly added to trajectiory X and Y positions to actually draw the animation of a shot.

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -1,5 +1,75 @@
 const canvas = document.querySelector('canvas');
-
+const context = canvas.getContext('2d');
+const projectiles = [];
 canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const x = canvas.width / 2;
+const y = canvas.height / 2;
 
-console.log(canvas);
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 360, false);//Math.PI * 2
+        context.fillStyle = this.color;
+        context.fill();
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    getY() {
+        return this.y;
+    }
+}
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 360, false);//Math.PI * 2
+        context.fillStyle = this.color;
+        context.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
+const player = new Player(x, y, 30, 'green');
+player.draw();
+// const projectile = new Projectile(player.getX(), player.getY(), 7, 'red', { x: 1, y: 1 });
+
+function animate() {
+    requestAnimationFrame(animate);
+    projectiles.forEach(p => {
+        p.update();
+    });
+    // projectile.draw();
+    // projectile.update();
+}
+
+window.addEventListener('click', (e) => {
+    const angleRad = Math.atan2(e.clientY - player.getY(), e.clientX - player.getX());
+    console.log(angleRad);
+    const velocity = { x: Math.cos(angleRad), y: Math.sin(angleRad) };
+    projectiles.push(new Projectile(player.getX(), player.getY(), 7, 'red', velocity));
+});
+
+animate();
