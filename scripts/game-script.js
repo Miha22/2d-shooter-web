b6d723ade558b31ece0be055fd887dd844756dda
Deniel Alekseev <daniel.neo.eu@icloud.com>
2022-02-27 05:20:45 +0100

    Added moveable player feature for that reason need to create two listeners for keydown and keyup then 4 boolean checkers what keys pressed and then simply upon them animate the next position of a player.

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -12,6 +12,10 @@ class Player {
         this.y = y;
         this.radius = radius;
         this.color = color;
+        this.moveUp = false;
+        this.moveDown = false;
+        this.moveRight = false;
+        this.moveLeft = false;
     }
 
     draw() {
@@ -19,6 +23,22 @@ class Player {
         context.arc(this.x, this.y, this.radius, 0, 360, false);//Math.PI * 2
         context.fillStyle = this.color;
         context.fill();
+    }
+
+    update() {
+        this.draw();
+        if(this.moveUp) {
+            this.y = this.y - 1;
+        }
+        if(this.moveDown) {
+            this.y = this.y + 1;
+        }
+        if(this.moveRight) {
+            this.x = this.x + 1;
+        }
+        if(this.moveLeft) {
+            this.x = this.x - 1;
+        }
     }
 
     getX() {
@@ -53,16 +73,60 @@ class Projectile {
 }
 
 const player = new Player(x, y, 30, 'green');
-player.draw();
 // const projectile = new Projectile(player.getX(), player.getY(), 7, 'red', { x: 1, y: 1 });
+
+function clearTrail() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 function animate() {
     requestAnimationFrame(animate);
+    clearTrail();
+    //player.draw();
+    player.update();
     projectiles.forEach(p => {
         p.update();
     });
-    // projectile.draw();
-    // projectile.update();
+}
+
+function pressedUp() {
+    player.moveUp = true;
+    console.log('Pressed UP');
+}
+
+function pressedDown() {
+    player.moveDown = true;
+    console.log('Pressed DOWN');
+}
+
+function pressedRight() {
+    player.moveRight = true;
+    console.log('Pressed RIGHT');
+}
+
+function pressedLeft() {
+    player.moveLeft = true;
+    console.log('Pressed LEFT');
+}
+
+function releasedUp() {
+    player.moveUp = false;
+    console.log('Released UP');
+}
+
+function releasedDown() {
+    player.moveDown = false;
+    console.log('Released DOWN');
+}
+
+function releasedRight() {
+    player.moveRight = false;
+    console.log('Released RIGHT');
+}
+
+function releasedLeft() {
+    player.moveLeft = false;
+    console.log('Released LEFT');
 }
 
 window.addEventListener('click', (e) => {
@@ -70,6 +134,70 @@ window.addEventListener('click', (e) => {
     console.log(angleRad);
     const velocity = { x: Math.cos(angleRad), y: Math.sin(angleRad) };
     projectiles.push(new Projectile(player.getX(), player.getY(), 7, 'red', velocity));
+});
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+    const { code } = event;
+    if (code === "KeyW") { // KeyA, KeyS, KeyD
+        pressedUp();
+    }
+    if (code === "KeyS") { // KeyA, KeyS, KeyD
+        pressedDown();
+    }
+    if (code === "KeyD") { // KeyA, KeyS, KeyD
+        pressedRight();
+    }
+    if (code === "KeyA") { // KeyA, KeyS, KeyD
+        pressedLeft();
+    }
+
+    switch (key) {
+        case "ArrowLeft":
+            pressedLeft();
+            break;
+        case "ArrowRight":
+            pressedRight();
+            break;
+        case "ArrowUp":
+            pressedUp();
+            break;
+        case "ArrowDown":
+            pressedDown();
+            break;
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+    const { code } = event;
+    if (code === "KeyW") { // KeyA, KeyS, KeyD
+        releasedUp();
+    }
+    if (code === "KeyS") { // KeyA, KeyS, KeyD
+        releasedDown();
+    }
+    if (code === "KeyD") { // KeyA, KeyS, KeyD
+        releasedRight();
+    }
+    if (code === "KeyA") { // KeyA, KeyS, KeyD
+        releasedLeft();
+    }
+
+    switch (key) {
+        case "ArrowLeft":
+            pressedLeft();
+            break;
+        case "ArrowRight":
+            pressedRight();
+            break;
+        case "ArrowUp":
+            pressedUp();
+            break;
+        case "ArrowDown":
+            pressedDown();
+            break;
+    }
 });
 
 animate();
